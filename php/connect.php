9646c269a21727766300e3711dcddb4055f2e4d9
Gabriel Freire <gabrbfreire@gmail.com>
2020-09-03 19:13:28 -0300

    env. variables

--- php/connect.php
+++ php/connect.php
@@ -1,8 +1,13 @@
 <?php
 $servername = getenv('DB_URL');
-$username = getenv('DB_USERNAME');;
-$password = getenv('DB_PASSWORD');;
-$dbname = getenv('DB_NAME');;
+$username = getenv('DB_USERNAME');
+$password = getenv('DB_PASSWORD');
+$dbname = getenv('DB_NAME');
+
+echo $servername;
+echo $username;
+echo $username;
+echo $dbname;
 
 $connection = mysqli_connect($servername, $username, $password, $dbname)or die("Error");
 mysqli_set_charset($connection, 'utf8');
