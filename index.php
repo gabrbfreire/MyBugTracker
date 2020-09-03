75ebe52b3079f37629b6eb18fa40f9a9a1e1b16b
Gabriel Freire <gabrbfreire@gmail.com>
2020-09-03 19:06:02 -0300

    hr

--- index.php
+++ index.php
@@ -61,7 +61,6 @@
                       Login
                     </button>
                   </form>
-                  <hr>
                   <div class="text-center">
                     <a class="small" href="forgot-password.html">Forgot Password?</a>
                   </div>
